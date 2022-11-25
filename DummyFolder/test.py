66b088d33b03c453d4a3313fda6ab37e703e828f
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-11-25 13:00:03 +0530

    asasa

--- DummyFolder/test.py
+++ DummyFolder/test.py
@@ -1,3 +1,6 @@
+import sys
 
-result = "test from python"
-print(f'::set-output name=test_report::{result}')
+print("argument {}".format(sys.argv[1]))
+print("changelog 1")
+print("changelog 2")
+print("changelog 3")
